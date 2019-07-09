7d81046b9cba3f25668e09fb39253cb689243ff6
Safin Timur <timurtlt96@mail.ru>
2019-07-09 13:59:27 +0300

    Added console interface

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,3 +1,11 @@
+import argparse
+import os.path
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(prefix_chars='-+/')
+    parser.add_argument('file', nargs='?', default=False, help='Path to json file')
+    return parser
 
 
 def load_data(filepath):
@@ -8,5 +16,18 @@ def get_most_frequent_words(text):
     pass
 
 
+def main():
+    parser = create_parser()
+    namespace = parser.parse_args()
+
+    if not namespace.file:
+        exit('Укажите путь к файлу.')
+    if not os.path.isfile(namespace.file):
+        exit('Такого файла не существует')
+
+    text = load_data(namespace.file)
+    get_most_frequent_words(text)
+
+
 if __name__ == '__main__':
-    pass
+    main()
